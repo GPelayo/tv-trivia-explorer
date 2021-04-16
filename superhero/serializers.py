acecf97e53c916225bf308e78793fa65f245adcc
Geryl Pelayo <gpelayo109@gmail.com>
2021-04-16 13:23:19 -0700

    Updated serializers to handle lists of model objects

--- superhero/serializers.py
+++ superhero/serializers.py
@@ -1,26 +1,45 @@
-from superhero import models
+from superhero.models import Trivia, Episode, Series
 from rest_framework import serializers
 
 from superhero import helpers
 
+import logging
+
 
 class TriviaSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = models.Trivia
+        model = Trivia
         fields = helpers.get_fields_from_model_name('Trivia')
 
 
 class EpisodeSerializer(serializers.HyperlinkedModelSerializer):
-    trivia = TriviaSerializer(many=True)
+    trivia_set = TriviaSerializer(many=True)
 
     class Meta:
-        model = models.Episode
-        fields = helpers.get_fields_from_model_name('Episode') + ['trivia']
+        model = Episode
+        fields = helpers.get_fields_from_model_name('Episode') + ['trivia_set']
+
+    def create(self, validated_data):
+        trivia_data = validated_data.pop('trivia_set')
+        episode = Episode.objects.create(**validated_data)
+        for trivia_json in trivia_data:
+            Trivia.objects.create(episode=episode, **trivia_json)
+        return episode
 
 
 class SeriesSerializer(serializers.HyperlinkedModelSerializer):
-    episodes = EpisodeSerializer(many=True)
+    episode_set = EpisodeSerializer(many=True)
 
     class Meta:
-        model = models.Series
-        fields = helpers.get_fields_from_model_name('Series') + ['episodes']
+        model = Series
+        fields = helpers.get_fields_from_model_name('Series') + ['episode_set']
+
+    def create(self, validated_data):
+        episode_data = validated_data.pop('episode_set')
+        series = Series.objects.create(**validated_data)
+        for episode_json in episode_data:
+            trivia_data = episode_json.pop('trivia_set')
+            episode = Episode.objects.create(series=series, **episode_json)
+            for trivia_json in trivia_data:
+          A      Trivia.objects.create(episode=episode, **trivia_json)
+        return series
