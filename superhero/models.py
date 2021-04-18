4aa17d594c6b181e6c42c96a547fde66f129a001
Geryl Pelayo <gpelayo109@gmail.com>
2021-04-17 20:19:00 -0700

    Explicitly declared primary keys

--- superhero/models.py
+++ superhero/models.py
@@ -3,18 +3,21 @@ from django.core.validators import MinValueValidator
 
 
 class Series(models.Model):
+    series_id = models.TextField(primary_key=True)
     name = models.TextField()
     season_count = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1)])
     thumbnail_url = models.URLField()
 
 
 class Episode(models.Model):
+    episode_id = models.TextField(primary_key=True)
     name = models.TextField()
     season = models.TextField()
     series = models.ForeignKey(Series, on_delete=models.CASCADE)
 
 
 class Trivia(models.Model):
+    trivia_id = models.TextField(primary_key=True)
     score = models.PositiveIntegerField()
     score_denominator = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1)])
     text = models.TextField()
