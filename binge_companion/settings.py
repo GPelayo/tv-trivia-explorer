bd67191c7773565922478f6804f25cf02b2b00d2
Geryl Pelayo <gpelayo109@gmail.com>
2021-06-22 14:06:50 -0700

    Created github actions workflow settings

--- binge_companion/settings.py
+++ binge_companion/settings.py
@@ -32,6 +32,7 @@ except ImportError:
 SECRET_KEY = DJANGO_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('IS_DEBUG', 'false').lower() == 'true'
+CI_TEST_MODE = os.environ.get('CI_TEST_MODE', False)
 
 ALLOWED_HOSTS = ['*']
 
@@ -85,16 +86,26 @@ WSGI_APPLICATION = 'binge_companion.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.path.exists(BASE_DIR / 'db.sqlite3'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-} if os.path.exists(BASE_DIR / 'db.sqlite3') else {
-    'default': dj_database_url.config(conn_max_age=600)
-}
-
+elif CI_TEST_MODE:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'PORT': '5432',
+            'HOST': '127.0.0.1',
+        }
+    }
+else:
+    DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
